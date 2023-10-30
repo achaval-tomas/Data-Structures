190d022cc11ae83bd86bd61540dcc732416d5ccf
achaval-tomas <tomiachaval2013@gmail.com>
2023-10-30 12:33:00 -0300

    added main structure for testing

--- Linked-List/testLinkedList.c
+++ Linked-List/testLinkedList.c
@@ -1,1 +1,13 @@
 /* TO-DO, create main to test functions */
+#include <stdio.h>
+#include <stdbool.h>
+#include "linked-list.h"
+
+int main (void){
+
+    MyLinkedList* tester = createList();
+
+    // Add tests.
+
+    return 0;
+}
