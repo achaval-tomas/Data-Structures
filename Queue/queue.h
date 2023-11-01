665b14a3584db81f2b3d85a473c80b3c24f4695d
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-01 20:38:22 -0300

    Completed Queue Specification

--- Queue/queue.h
+++ Queue/queue.h
@@ -1,1 +1,35 @@
 /*  My Queue Specification  */
+
+#ifndef QUEUE_HEADER
+#define QUEUE_HEADER
+
+#include <stdlib.h>
+
+typedef struct s_queue MyQueue;
+
+/*  INITIALIZE QUEUE OBJECT  */
+MyQueue* q_newQueue(void);
+
+/*  ENQUEUE elem  */
+MyQueue* q_enqueue(MyQueue* q, int elem);
+
+/*  DEQUEUE  */
+void q_dequeue(MyQueue* q);
+
+/*  RETURNS THE FIRST ELEMENT IN THE QUEUE  */
+int q_front(MyQueue* q);
+
+/*  RETURNS SIZE OF QUEUE  */
+int q_size(MyQueue* q);
+
+/*  RETURNS 1 IF QUEUE IS EMPTY, 0 otherwise  */
+int q_isEmpty(MyQueue* q);
+
+/*  FREE ALL SPACE USED BY QUEUE  */
+void q_free(MyQueue* q);
+
+/*  PRINT QUEUE TO stdout  */
+void q_print(MyQueue* q);
+
+
+#endif /* QUEUE_HEADER */
