fb4be8994c8617b4bd88e7f88e6dd30f05c24b7d
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-01 15:23:22 -0300

    Completed set specification

--- Set/set.h
+++ Set/set.h
@@ -1,1 +1,32 @@
 /* My set Specification */
+
+#ifndef SET_HEADER
+#define SET_HEADER
+
+#include <stdlib.h>
+
+typedef struct s_set MySet;
+
+/*  CREATE A NEW EMPTY SET  */
+MySet* newSet();
+
+/*  RETURNS THE AMOUNT OF ELEMENTS IN THE SET  */
+int setSize(MySet* set);
+
+/*  ADD ELEMENT TO THE SET  */
+void addTo(MySet* set, int elem);
+
+/*  REMOVE ELEMENT FROM THE SET  */
+void removeFrom(MySet* set, int elem);
+
+/*  CHECK IF elem EXISTS IN THE SET  */
+bool exists(MySet* set, int elem);
+
+/*  FREE ALL SPACE USED BY SET  */
+void freeSet(MySet* set);
+
+/*  PRINT THE SET TO stdout  */
+void printSet(MySet* set);
+
+
+#endif /* SET_HEADER */
