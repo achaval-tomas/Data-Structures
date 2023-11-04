cd7da80696353066d8948345294158a49e707f13
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-03 21:22:40 -0300

    Start work on tree implementation

--- Trees/Binary-Tree/binaryTree.c
+++ Trees/Binary-Tree/binaryTree.c
@@ -1,6 +1,7 @@
 /*  My Binary Tree Implementation  */
 
 #include "binaryTree.h"
+#include <assert.h>
 
 struct s_btree {
     int val;
@@ -9,19 +10,54 @@ struct s_btree {
 };
 
 /*  CREATE A NEW TREE OBJECT  */
-MyTree* newTree(void){}
+MyTree* newTree(void){
+    MyTree* t = malloc(sizeof(*t));
+    t->val = -1;
+    t->left = t;
+    t->right = t;
+    return t;
+}
 
 /*  RETURNS THE AMOUNT OF NODES IN THE TREE  */
-int treeSize(MyTree* tree){}
+int treeSize(MyTree* tree){
+    return isEmpty(tree) ? 0 : 1+treeSize(tree->left)+treeSize(tree->right);
+}
 
 /*  ADD NODE TO THE TREE  */
-void addNode(MyTree* tree){}
+void addNode(MyTree* tree, int elem){
+    assert(tree);    
+    if (isEmpty(tree)){
+        tree->val = elem;
+        tree->left = NULL;
+        tree->right = NULL;
+    } else {
+        if (tree->left && !tree->right){
+            tree->right = newTree();
+            addNode(tree->right, elem);
+        } else if (!tree->left && tree->right){
+            tree->left = newTree();
+            addNode(tree->left, elem);
+        } else {
+            if (treeSize(tree->left) < treeSize(tree->right))
+                addNode(tree->left, elem);
+            else
+                addNode(tree->right, elem);
+        }
+    }
+}
 
 /*  REMOVE ELEM FROM THE TREE  */
 void remove(MyTree* tree, int elem){}
 
 /*  CHECK IF ELEM EXISTS IN TREE  */
-bool exists(MyTree* tree, int elem){}
+bool exists(MyTree* tree, int elem){
+    return isEmpty(tree) ? false : (tree->val == elem) || exists(tree->left, elem) || exists(tree->right, elem);
+}
+
+/*  DETERMINES IF TREE IS EMPTY  */
+bool isEmpty(MyTree* tree){
+    return tree == NULL || (tree->left == tree->right);
+}
 
 /*  ORDER FUNCTIONS
  *  Returned array must be freed by caller.
@@ -33,7 +69,20 @@ int* postOrderTraversal(MyTree* tree){}
 int* inOrderTraversal(MyTree* tree){}
 
 /*  FREE ALL SPACE USED BY TREE  */
-void freeTree(MyTree* tree){}
+void freeTree(MyTree* tree){
+    if (!tree)
+        return;
+    if (isEmpty(tree)){
+        free(tree->right);
+        free(tree->left);
+        free(tree);
+        return;
+    }
+    MyTree* killme = tree;
+    freeTree(tree->left);
+    freeTree(tree->right);
+    free(killme);
+}
 
 /*  PRINT TREE  */
 void printTree(MyTree* tree, order_t type){}
--- Trees/Binary-Tree/binaryTree.h
+++ Trees/Binary-Tree/binaryTree.h
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct s_btree MyTree;
 
@@ -15,13 +16,16 @@ MyTree* newTree(void);
 int treeSize(MyTree* tree);
 
 /*  ADD NODE TO THE TREE  */
-void addNode(MyTree* tree);
+void addNode(MyTree* tree, int elem);
 
 /*  REMOVE ELEM FROM THE TREE  */
 void remove(MyTree* tree, int elem);
 
 /*  CHECK IF ELEM EXISTS IN TREE  */
 bool exists(MyTree* tree, int elem);
+
+/*  DETERMINES IF TREE IS EMPTY  */
+bool isEmpty(MyTree* tree);
 
 /*  ORDER FUNCTIONS
  *  Returned array must be freed by caller.
