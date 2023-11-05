e5465c314edfab582434e235190936c408bbe23a
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-05 20:08:10 -0300

    Initial structure for graph header

--- Graphs/Normal-Graphs/graph.h
+++ Graphs/Normal-Graphs/graph.h
@@ -1,1 +1,30 @@
 /*  My Graphs Specification  */
+
+#ifndef GRAPH_HEADER
+#define GRAPH_HEADER
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct s_graph MyGraph;
+
+/*  CREATE A NEW EMPTY GRAPH  */
+MyGraph* newGraph(void);
+
+/*  ADD A VERTEX TO GRAPH  */
+void addVertex(MyGraph* graph, int vertex, int* connections, int connectionsSize);
+
+/*  REMOVE A VERTEX FROM THE GRAPH  */
+void removeVertex(MyGraph* graph, int vertex);
+
+/*  RETURN ALL CONNECTIONS OF A VERTEX  */
+int* connections(MyGraph* graph, int vertex);
+
+/*  FREE ALL SPACE USED BY graph  */
+void freeGraph(MyGraph* graph);
+
+/*  PRINT GRAPH TO stdout  */
+void printGraph(MyGraph* graph);
+
+#endif /*  GRAPH_HEADER  */
