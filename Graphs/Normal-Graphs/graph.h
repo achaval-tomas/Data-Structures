42f9aa945179d829f133a14148064098849c882c
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-05 20:35:16 -0300

    Completed graph specification

--- Graphs/Normal-Graphs/graph.h
+++ Graphs/Normal-Graphs/graph.h
@@ -12,14 +12,26 @@ typedef struct s_graph MyGraph;
 /*  CREATE A NEW EMPTY GRAPH  */
 MyGraph* newGraph(void);
 
-/*  ADD A VERTEX TO GRAPH  */
-void addVertex(MyGraph* graph, int vertex, int* connections, int connectionsSize);
+/*  RETURNS THE AMOUNT OF UNIQUE VERTICES IN THE GRAPH  */
+int graphSize(MyGraph* graph);
 
-/*  REMOVE A VERTEX FROM THE GRAPH  */
-void removeVertex(MyGraph* graph, int vertex);
+/*  ADD EDGE [v1, v2] TO GRAPH  */
+void addEdge(MyGraph* graph, int v1, int v2);
 
-/*  RETURN ALL CONNECTIONS OF A VERTEX  */
-int* connections(MyGraph* graph, int vertex);
+/*  REMOVE EDGE [v1, v2] FROM THE GRAPH  */
+void removeEdge(MyGraph* graph, int v1, int v2);
+
+/*  REMOVE THE VERTEX v (AND ALL OF ITS CONNECTIONS) FROM THE GRAPH  */
+void removeVertex(MyGraph* graph, int v);
+
+/*  RETURN ALL CONNECTIONS OF A VERTEX
+ *  loads the number of connections into retSize.
+ *  returned array must be freed by the caller.
+ */
+int* connections(MyGraph* graph, int vertex, int* retSize);
+
+/*  DETERMINES IF [v1, v2] IS AN EDGE OF THE GRAPH  */
+bool isEdge(MyGraph* graph, int v1, int v2);
 
 /*  FREE ALL SPACE USED BY graph  */
 void freeGraph(MyGraph* graph);
