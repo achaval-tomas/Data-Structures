98eaf1333aded5eb1e3a7c6468607214c97b858e
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-06 15:02:53 -0300

    Some work on UI

--- UI/userInterface.c
+++ UI/userInterface.c
@@ -2,12 +2,39 @@
  *  This is a console interface to play around with
  *  the different data structures in the library.
  */
+#include <stdio.h>
+#include "handlers.h"
 
-/* TO-DO
- *  - Main Menu
- *  - Menu for each DS
- */
-int main(int argc, int* argv[]){
+void printMenu(){
+    fprintf(stdout, "\n- MAIN MENU\n"
+                    "SELECT AN OPTION:\n"
+                    "\tArray: 'a'\n"
+                    "\tSet: 's'\n"
+                    "\tQueue: 'q'\n"
+                    "\tStack: 'k'\n"
+                    "\tTree: 't'\n"
+                    "\tGraph: 'g'\n"
+                    "\tExit: 'e'\n"
+                    "\tSelect Option: ");
+    fflush(stdout);
+}
+
+int main(int argc, char* argv[]){
+    char quit = 0;
+    while (!quit){
+        printMenu();
+        switch(getchar()){
+            case 'a':
+                handleArray();
+                break;
+            case 'e':
+                fflush(stdin);
+                fflush(stdout);
+                quit = 1;
+            default:
+                break;
+        }
+    }
 
     return 0;
 }
