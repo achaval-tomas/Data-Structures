36cbe9bed45019102e60c0735469b0d1f14e8d7c
achaval-tomas <tomiachaval2013@gmail.com>
2023-11-06 12:18:07 -0300

    Added folder for future User Interface

---
+++ UI/userInterface.c
@@ -0,0 +1,13 @@
+/*
+ *  This is a console interface to play around with
+ *  the different data structures in the library.
+ */
+
+/* TO-DO
+ *  - Main Menu
+ *  - Menu for each DS
+ */
+int main(int argc, int* argv[]){
+
+    return 0;
+}
